74e848333dbc91e99dc3cfebd61d8a21e0e4bd5c
Jenny <bylee94@hotmail.com>
2016-05-25 20:21:01 +1200

    findTitle, findAuthor methods

--- PDFExtractPrototype/src/helperClasses/FontGroupBlock.java
+++ PDFExtractPrototype/src/helperClasses/FontGroupBlock.java
@@ -4,11 +4,13 @@ public class FontGroupBlock {
 	private String font;
 	private float fontSize;
 	private String text;
+	private int pageNum;
 	
-	public FontGroupBlock(String font, float fontSize, String text){
+	public FontGroupBlock(String font, float fontSize, String text, int pageNum){
 		this.font = font;
 		this.fontSize = fontSize;
 		this.text = text;
+		this.pageNum = pageNum;
 	}
 	
 	public String getFont(){
@@ -21,5 +23,9 @@ public class FontGroupBlock {
 	
 	public String getText(){
 		return this.text;
+	}
+	
+	public int getPageNum(){
+		return this.pageNum;
 	}
 }
--- PDFExtractPrototype/src/reportCheckers/UOAReportChecker.java
+++ PDFExtractPrototype/src/reportCheckers/UOAReportChecker.java
@@ -2,30 +2,71 @@ package reportCheckers;
 
 import java.util.ArrayList;
 
+import helperClasses.FontGroupBlock;
+
 public class UOAReportChecker implements ReportChecker {
 
-	ArrayList<String> fontGroupings = new ArrayList<String>();
+	ArrayList<FontGroupBlock> fontGroupings = new ArrayList<FontGroupBlock>();
+	FontGroupBlock titleFGB = new FontGroupBlock(null, 0, null, 0);
 	
-	public UOAReportChecker(ArrayList<String> fontGroupings){
+	public UOAReportChecker(ArrayList<FontGroupBlock> fontGroupings){
 		this.fontGroupings = fontGroupings;
 	}
 	
 	@Override
-	public boolean isTitle(String content, String font, float fontSize) {
-		// TODO Auto-generated method stub
-		return false;
+	public String findTitle() {
+		float max = maxFontSize();
+		String title = "";
+		for (FontGroupBlock fgb : this.fontGroupings){
+			if(fgb.getFontSize() == max && fgb.getPageNum() <= 3){
+				this.titleFGB = fgb;
+				title = fgb.getText();
+			}
+		}
+		return title;
 	}
 
 	@Override
-	public boolean isAuthor(String content, String font, float fontSize) {
-		// TODO Auto-generated method stub
-		return false;
+	public String findAuthor() {
+		String author = "";
+		int titleIndex = 0;
+		boolean isName;
+		for (int i=0; i< this.fontGroupings.size(); i++){
+			if(this.fontGroupings.get(i) == this.titleFGB){
+				titleIndex = i;
+			}		
+		}
+		for (int i=titleIndex + 1; i< this.fontGroupings.size(); i++){
+			isName = true;
+			author = this.fontGroupings.get(i).getText();
+			String authorName[] = author.split(" ");
+				
+			for(String name : authorName){
+				if (!Character.isUpperCase(name.charAt(0))){
+					isName = false;
+				}
+			}
+			if(isName){
+				return author;
+			}
+		}
+		return null;
 	}
 
 	@Override
-	public boolean isAbstract(String content, String font, float fontSize) {
+	public String findAbstract() {
 		// TODO Auto-generated method stub
-		return false;
+		return null;
 	}
-
+	
+	public float maxFontSize() {
+		float maxFontSize = 0;
+		
+		for(FontGroupBlock fg : this.fontGroupings) {
+			if (fg.getFontSize() >= maxFontSize){
+				maxFontSize = fg.getFontSize();
+			}
+		}		
+		return maxFontSize;
+	}
 }
